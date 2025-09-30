ec56dc4dee4bec939aed61d5c2db66e3578407af
Norman <rakov389@gmail.com>
2025-09-30 22:27:09 +0300

    Added tests for Auth Service Register, Logout, ValidateToken methods

--- internal/services/auth/auth_test.go
+++ internal/services/auth/auth_test.go
@@ -5,6 +5,7 @@ import (
 	domain_errors "auth-service/internal/domain/errors"
 	"auth-service/internal/domain/models"
 	"context"
+	"errors"
 	"testing"
 	"time"
 
@@ -58,6 +59,11 @@ func (m *MockUserSaver) SaveUser(
 	lastName string,
 ) (int64, error) {
 	args := m.Called(ctx, email, username, string(passHash), firstName, lastName)
+
+	if args.Get(0) == 0 {
+		return 0, args.Error(1)
+	}
+
 	return args.Get(0).(int64), args.Error(1)
 }
 
@@ -66,15 +72,20 @@ func (m *MockCache) StoreToken(key string, value int64, ttl time.Duration) {
 }
 
 func (m *MockCache) RemoveToken(key string) error {
-	m.Called(key)
-	return nil
+	args := m.Called(key)
+	return args.Error(0)
 }
 
 func (m *MockTokenProvider) NewToken(user *models.User) (string, time.Duration, error) {
 	args := m.Called(user)
 	if args.Get(0) == nil {
-		return "", 0, args.Error(1)
+		return "", 0, args.Error(2)
 	}
+
+	if args.Get(1) == 0 {
+		return "", 0, args.Error(2)
+	}
+
 	return args.Get(0).(string), args.Get(1).(time.Duration), args.Error(2)
 }
 
@@ -139,6 +150,88 @@ func (suite *AuthTestSuite) TestAuth_Login_UserNotFound() {
 	suite.Error(err)
 	suite.Empty(token)
 	suite.mockUserSaver.AssertNotCalled(suite.T(), "SaveUser")
+}
+
+func (suite *AuthTestSuite) TestAuth_Login_TokenError() {
+	suite.mockUserProvider.On("GetUser", suite.ctx, suite.expectedUser.Email).Return(suite.expectedUser, nil)
+	suite.mockCache.On("StoreToken", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+	suite.mockjwtService.On("NewToken", suite.expectedUser).Return("", 0, errors.New("some error"))
+
+	token, err := suite.authService.Login(suite.ctx, "john_doe@test.com", "password")
+
+	suite.Error(err)
+	suite.Empty(token)
+	suite.mockUserSaver.AssertExpectations(suite.T())
+	suite.mockjwtService.AssertExpectations(suite.T())
+	suite.mockCache.AssertNotCalled(suite.T(), "StoreToken")
+}
+
+func (suite *AuthTestSuite) TestAuth_Login_TokenValid() {
+	suite.mockjwtService.On("ValidateToken", mock.Anything).Return(suite.expectedUser.ID)
+	uid := suite.authService.ValidateToken("test")
+
+	suite.NotNil(uid)
+	suite.IsType(int64(0), uid)
+}
+
+func (suite *AuthTestSuite) TestAuth_Login_RegisterSuccess() {
+	suite.mockUserSaver.On(
+		"SaveUser",
+		suite.ctx,
+		suite.expectedUser.Email,
+		suite.expectedUser.Username,
+		mock.Anything,
+		suite.expectedUser.FirstName,
+		suite.expectedUser.LastName).Return(suite.expectedUser.ID, nil)
+
+	uid, err := suite.authService.Register(
+		suite.ctx,
+		suite.expectedUser.Email,
+		suite.expectedUser.Username,
+		"password",
+		suite.expectedUser.FirstName,
+		suite.expectedUser.LastName,
+	)
+	suite.NoError(err)
+	suite.NotNil(uid)
+	suite.NotEmpty(uid)
+}
+
+func (suite *AuthTestSuite) TestAuth_Login_SaveUserError() {
+	suite.mockUserSaver.On(
+		"SaveUser",
+		mock.Anything,
+		mock.Anything,
+		mock.Anything,
+		mock.Anything,
+		mock.Anything,
+		mock.Anything).Return(0, errors.New("some error"))
+
+	uid, err := suite.authService.Register(
+		suite.ctx,
+		suite.expectedUser.Email,
+		suite.expectedUser.Username,
+		"password",
+		suite.expectedUser.FirstName,
+		suite.expectedUser.LastName,
+	)
+
+	suite.Error(err)
+	suite.Equal(int64(0), uid)
+}
+
+func (suite *AuthTestSuite) TestAuth_Login_LogoutSuccess() {
+	suite.mockCache.On("RemoveToken", mock.Anything).Return(nil)
+
+	err := suite.authService.Logout("token")
+	suite.NoError(err)
+}
+
+func (suite *AuthTestSuite) TestAuth_Login_LogoutFail() {
+	suite.mockCache.On("RemoveToken", mock.Anything).Return(errors.New("some error"))
+
+	err := suite.authService.Logout("token")
+	suite.Error(err)
 }
 
 func TestAuthTestSuite(t *testing.T) {
